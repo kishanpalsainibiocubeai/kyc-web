fa0ae0fd8a22dcd83791aa520ca999d64816d0f4
Kishanpal Saini <kishanpal.saini@Biocube-Matrics.local>
2024-07-03 17:32:45 +0530

    color theme

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import './CustomTheme.css'
 
 function App() {
   return (
@@ -28,7 +29,6 @@ function App() {
       <Box className='layout-main-footer-wrapper'>
       {/* <Footer /> */}
       </Box>
-
 
       <ToastContainer />
       <Routes>
